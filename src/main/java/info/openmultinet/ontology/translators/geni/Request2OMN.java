42e55c5ecf093cc39663573e16369f6c24df931f
Alexander Willner <alexander.willner@tu-berlin.de>
2015-01-15 16:06:35 +0100

    added initial rspec2omn implementation

--- src/main/java/info/openmultinet/ontology/translators/geni/Request2OMN.java
+++ src/main/java/info/openmultinet/ontology/translators/geni/Request2OMN.java
@@ -1,12 +1,58 @@
 package info.openmultinet.ontology.translators.geni;
 
+import info.openmultinet.ontology.translators.geni.jaxb.request.NodeContents;
+import info.openmultinet.ontology.translators.geni.jaxb.request.RSpecContents;
+import info.openmultinet.ontology.vocabulary.Omn_lifecycle;
+
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
+
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.RDF;
 
 public class Request2OMN {
 
-	public static Model getModel(String string) {
-		// TODO Auto-generated method stub
-		return null;
+	public static Model getModel(InputStream input) throws JAXBException {
+		
+		JAXBContext context = JAXBContext.newInstance(RSpecContents.class);
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+		JAXBElement<RSpecContents> rspec = unmarshaller.unmarshal(new StreamSource(input), RSpecContents.class);
+		RSpecContents request = rspec.getValue();
+
+		Model model = ModelFactory.createDefaultModel();
+		Resource topology = model.createResource("http://open-multinet.info/example#request");
+		topology.addProperty(RDF.type, Omn_lifecycle.Request);
+		
+		
+		//@todo: on purpose clean and small - have to check exceptions to get links and others
+		List<JAXBElement<NodeContents>> nodes = (List) request.getAnyOrNodeOrLink();
+		for (JAXBElement<NodeContents> nodeObject : nodes) {
+			NodeContents node = nodeObject.getValue();
+			model.createResource(node.getClientId());
+		}
+		
+		return model;
 	}
 
+	private static String toString(Object jaxbObject) throws JAXBException {
+		JAXBContext jaxbContext = JAXBContext
+				.newInstance("info.openmultinet.ontology.translators.geni.jaxb.request");
+		Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+		jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		StringWriter stringWriter = new StringWriter();
+		jaxbMarshaller.marshal(jaxbObject, stringWriter);
+
+		return stringWriter.toString();
+	}
 }
+
--- src/test/java/info/openmultinet/ontology/translators/geni/Request2OMNTest.java
+++ src/test/java/info/openmultinet/ontology/translators/geni/Request2OMNTest.java
@@ -1,14 +1,26 @@
 package info.openmultinet.ontology.translators.geni;
 
+import info.openmultinet.ontology.vocabulary.Omn_lifecycle;
+
+import java.io.InputStream;
+
+import javax.xml.bind.JAXBException;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ResIterator;
+import com.hp.hpl.jena.vocabulary.RDF;
 
 public class Request2OMNTest {
 
 	@Test
-	public void test() {
-		Model model = Request2OMN.getModel("");
+	public void test() throws JAXBException {
+		InputStream rspec = Request2OMNTest.class.getResourceAsStream("/request.rspec");
+		Model model = Request2OMN.getModel(rspec);
+		ResIterator topology = model.listResourcesWithProperty(RDF.type, Omn_lifecycle.Request);
+		Assert.assertTrue("should have a topology", topology.hasNext());
 	}
 
 }
