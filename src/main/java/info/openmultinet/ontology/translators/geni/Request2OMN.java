b6f1e43555ad1e0d3b683f9d79ac8f8eee9de6c3
Alexander Willner <alexander.willner@tu-berlin.de>
2015-01-15 15:16:11 +0100

    starting request2omn

--- src/main/java/info/openmultinet/ontology/translators/geni/Request2OMN.java
+++ src/main/java/info/openmultinet/ontology/translators/geni/Request2OMN.java
@@ -1,5 +1,12 @@
 package info.openmultinet.ontology.translators.geni;
 
+import com.hp.hpl.jena.rdf.model.Model;
+
 public class Request2OMN {
+
+	public static Model getModel(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
---
+++ src/test/java/info/openmultinet/ontology/translators/geni/Request2OMNTest.java
@@ -0,0 +1,14 @@
+package info.openmultinet.ontology.translators.geni;
+
+import org.junit.Test;
+
+import com.hp.hpl.jena.rdf.model.Model;
+
+public class Request2OMNTest {
+
+	@Test
+	public void test() {
+		Model model = Request2OMN.getModel("");
+	}
+
+}
