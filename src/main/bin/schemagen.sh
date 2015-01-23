177c98f25eef852da03e411cb84e6b1689f39bbc
Alex <alex@willner.ws>
2015-01-23 16:12:06 +0100

    using maven now

--- src/main/bin/schemagen.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#TODO: use mvn schemagen:generate instead
-for i in $(ls src/main/resources/*.ttl); do 
-  schemagen --owl -o src/main/java/ -i $i --package "info.openmultinet.ontology.vocabulary" --ontology --inference;
-done
-
