66767f247d8db8c58439c2cf38302bd24e5a864b
mitjanikolaus <mitjanikolaus@gmail.com>
2015-02-10 13:57:45 +0100

    enabling osco related code again

--- src/test/java/info/openmultinet/ontology/translators/tosca/Tosca2OMNTest.java
+++ src/test/java/info/openmultinet/ontology/translators/tosca/Tosca2OMNTest.java
@@ -41,8 +41,7 @@ public class Tosca2OMNTest {
     Assert.assertTrue("Should contain parameter resources", model.containsResource(Osco.parameter2));
     Assert.assertTrue("parameter2 should be of range string", model.contains(Osco.parameter2, RDFS.range, XSD.xstring));
     Assert.assertTrue("minNumInst should be of range int", model.contains(Osco.minNumInst, RDFS.range, XSD.xint));
-    //@todo: couldn't find this property - also we'd this class twice in the class path
-    //Assert.assertTrue("Should contain the service properties", model.containsResource(Osco.TEST_PARAM));
+    Assert.assertTrue("Should contain the service properties", model.containsResource(Osco.TEST_PARAM));
   }
   
   @Test
