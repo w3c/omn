583429fbf031747d12185de56b40d103480cc140
Robyn Loughnane <loughnane@campus.tu-berlin.de>
2015-09-01 14:15:05 +0200

    try to fix build again

--- .travis/mvn_deploy.sh
+++ .travis/mvn_deploy.sh
@@ -2,7 +2,6 @@
 if [ x"$1" = "x-f" ] || [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [ ${TRAVIS_BRANCH} = 'master' ]; then
 	mvn -DskipTests --settings .travis/settings.xml \
 		deploy \
-		deploy:deploy-file -DrepositoryId=ossrh -Dfile=target/omnlib.jar -Durl=https://oss.sonatype.org/content/repositories/snapshots  -DgroupId=info.open-multinet  -DartifactId=omnlib -Dversion=0.0.1-SNAPSHOT
 else
 	true
 fi
