fdcf065ef78e11fb6e0a414353e6b88cccf69231
Alexander Willner <alexander.willner@tu-berlin.de>
2015-10-12 13:54:42 +0200

    starting to test rules

---
+++ omnlib/src/test/java/info/openmultinet/ontology/translators/geni/advertisement/TestRules.java
@@ -0,0 +1,36 @@
+package info.openmultinet.ontology.translators.geni.advertisement;
+
+import java.io.IOException;
+
+import org.apache.jena.riot.RDFDataMgr;
+import org.junit.Test;
+
+import com.hp.hpl.jena.rdf.model.InfModel;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.reasoner.Reasoner;
+import com.hp.hpl.jena.reasoner.rulesys.GenericRuleReasoner;
+import com.hp.hpl.jena.reasoner.rulesys.Rule;
+
+import info.openmultinet.ontology.translators.AbstractConverter;
+
+public class TestRules {
+
+	private static final String EXPECTED1 = "omn-monitoring-unit";
+	private static final String RAWFILE1 = "geni/geni-fire-20151006/netmode.rspec.xml.ttl";
+	private static final String RULEFILE1 = "/rules/rule1.txt";
+
+	@Test
+	public void test() throws IOException {
+
+		final String rule1 = AbstractConverter.toString(RULEFILE1);
+		Model rawModel1 = RDFDataMgr.loadModel(RAWFILE1) ;
+		
+		Reasoner reasoner = new GenericRuleReasoner(Rule.parseRules(rule1));
+		reasoner.setDerivationLogging(true);
+		InfModel infModel1 = ModelFactory.createInfModel(reasoner, rawModel1);
+		
+		//fixme Assert.assertTrue(infModel1.toString().contains(TestRules.EXPECTED1));
+	}
+
+}
