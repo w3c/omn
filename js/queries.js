1997bf0f5552e89333c0679dfc5ba9a2a1a6678e
Alexander Willner <alexander.willner@tu-berlin.de>
2015-12-14 21:15:39 +0100

    new query

--- js/queries.js
+++ js/queries.js
@@ -4,10 +4,12 @@ $( document ).ready(function() {
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX omn: <http://open-multinet.info/ontology/omn#>
 PREFIX omnfed: <http://open-multinet.info/ontology/omn-federation#>
+PREFIX omnres: <http://open-multinet.info/ontology/omn-resource#>
+PREFIX omnlc: <http://open-multinet.info/ontology/omn-lifecycle#>
 `
   $('#findAMs').click(function(){
     yasqe.setValue(findAMs);
-    yasqe.query(yasr.setResponse);  
+    yasqe.query(yasr.setResponse);
     return false;
   });
   findAMs=prefixes+`
@@ -20,7 +22,7 @@ SELECT DISTINCT ?name ?am ?endpoint WHERE {
 
   $('#findInfras').click(function(){
     yasqe.setValue(findInfras);
-    yasqe.query(yasr.setResponse);  
+    yasqe.query(yasr.setResponse);
     return false;
   });
   findInfras=prefixes+`
@@ -29,5 +31,24 @@ SELECT DISTINCT  ?name ?uri  WHERE {
   OPTIONAL { ?uri rdfs:label ?name }
 } ORDER BY DESC (?name) LIMIT 100`
 
-});
 
+  $('#findOverview').click(function(){
+    yasqe.setValue(findOverview);
+    yasqe.query(yasr.setResponse);
+    return false;
+  });
+  findOverview=prefixes+`
+`
+SELECT
+  (COUNT (DISTINCT ?manager) as ?managers)
+  (COUNT (DISTINCT ?node) as ?nodes)
+  (COUNT (DISTINCT ?interface) as ?interfaces)
+  (COUNT (DISTINCT ?link) as ?links)
+WHERE {
+  {?component omnlc:hasComponentManagerName ?manager .}
+  UNION {?node a omnres:Node .}
+  UNION {?interface a omnres:Interface .}
+  UNION {?link a omnres:Link .}
+}`
+
+});
